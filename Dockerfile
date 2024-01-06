c3967fe1bcb729908efd62f8279c04c7b856f0c1
MoyaF <faustomoya99@gmail.com>
2024-01-07 00:52:38 +0100

    feature: use pnpm

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 # syntax = docker/dockerfile:1
 
-# Adjust BUN_VERSION as desired
-ARG BUN_VERSION=1.0.21
-FROM oven/bun:${BUN_VERSION}-slim as base
+# Adjust NODE_VERSION as desired
+ARG NODE_VERSION=20.6.1
+FROM node:${NODE_VERSION}-slim as base
 
 LABEL fly_launch_runtime="Remix"
 
@@ -11,6 +11,10 @@ WORKDIR /app
 
 # Set production environment
 ENV NODE_ENV="production"
+
+# Install pnpm
+ARG PNPM_VERSION=8.7.5
+RUN npm install -g pnpm@$PNPM_VERSION
 
 
 # Throw-away build stage to reduce size of final image
@@ -21,27 +25,18 @@ RUN apt-get update -qq && \
     apt-get install --no-install-recommends -y build-essential node-gyp pkg-config python-is-python3
 
 # Install node modules
-COPY --link bun.lockb package.json ./
-RUN bun install
-
-# Update nodejs version
-ARG NODE_MAJOR=21
-RUN apt-get install -y ca-certificates curl gnupg
-RUN mkdir -p /etc/apt/keyrings
-RUN curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-RUN echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-RUN apt-get update
-RUN apt-get install nodejs -y
+COPY --link package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile --prod=false
 
 # Copy application code
 COPY --link . .
 
 # Build application
-RUN bun run --bun build
+RUN pnpm run build
 
 # Remove development dependencies
-RUN rm -rf node_modules && \
-    bun install --ci
+RUN pnpm prune --prod
+
 
 # Final stage for app image
 FROM base
@@ -51,4 +46,4 @@ COPY --from=build /app /app
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
-CMD [ "bun", "run", "start" ]
+CMD [ "pnpm", "run", "start" ]
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -11,6 +11,7 @@ import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
+import { IsBotProvider } from "./context/is-bot-context";
 
 const ABORT_DELAY = 5_000;
 
@@ -19,9 +20,7 @@ export default function handleRequest(
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line
   loadContext: AppLoadContext
 ) {
   return isbot(request.headers.get("user-agent"))
@@ -47,22 +46,24 @@ function handleBotRequest(
 ) {
   return new Promise((resolve, reject) => {
     let shellRendered = false;
+
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <IsBotProvider isBot={true}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </IsBotProvider>,
       {
         onAllReady() {
           shellRendered = true;
           const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
 
           resolve(
-            new Response(stream, {
+            new Response(createReadableStreamFromReadable(body), {
               headers: responseHeaders,
               status: responseStatusCode,
             })
@@ -98,21 +99,22 @@ function handleBrowserRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <IsBotProvider isBot={false}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </IsBotProvider>,
       {
         onShellReady() {
           shellRendered = true;
           const body = new PassThrough();
-          const stream = createReadableStreamFromReadable(body);
 
           responseHeaders.set("Content-Type", "text/html");
 
           resolve(
-            new Response(stream, {
+            new Response(createReadableStreamFromReadable(body), {
               headers: responseHeaders,
               status: responseStatusCode,
             })

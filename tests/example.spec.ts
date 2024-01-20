367cda4e1675ae2103a473f378bdbcef5a602536
MoyaF <faustomoya99@gmail.com>
2024-01-20 13:59:45 +0100

    feature: add e2e tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-const baseUrl = process.env.BASE_URL!;
 
 test("has title", async ({ page }) => {
-  await page.goto(baseUrl);
+  await page.goto("/");
   await expect(page).toHaveTitle(
     "ShirtScanner: Explore, Compare, and Find the Best Sports Clothes in China"
   );
@@ -11,20 +10,20 @@ test("has title", async ({ page }) => {
 
 test("can search", async ({ page }) => {
   const query = "Argentina";
-  await page.goto(baseUrl);
+  await page.goto("/");
   await page.getByPlaceholder("Find your next shirt...").fill(query);
   await page.getByRole("button", { name: "Search" }).click();
-  await expect(page).toHaveURL(`${baseUrl}/search?q=${query}`);
+  await expect(page).toHaveURL(`/search?q=${query}`);
 });
 
 test("has providers", async ({ page }) => {
-  await page.goto(`${baseUrl}/providers`);
+  await page.goto(`/providers`);
   const rowCount = await page.$$eval(`tr`, rows => rows.length);
   expect(rowCount).toBeGreaterThan(0);
 });
 
 test("providers redirects to providers page search", async ({ page }) => {
-  await page.goto(baseUrl);
+  await page.goto("/");
   await page.getByRole('link', { name: 'Providers' }).click();
-  await expect(page).toHaveURL(`${baseUrl}/providers`);
+  await expect(page).toHaveURL(`/providers`);
 });

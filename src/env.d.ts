ffad24f51465075324343f046bb10cb5e476fd27
moyis <fausto@moyis.dev>
2026-09-16 20:53:15 -0300

    feat(astro): port header + search bar + index page to static

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
   readonly PUBLIC_SHIRTSCANNER_BE: string | undefined;
+  readonly PUBLIC_POSTHOG_TOKEN: string | undefined;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
